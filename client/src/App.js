464d40708ad275ac373210d5bc3f6362f0965cb0
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-12-25 22:55:09 -0500

    client: add routes to the App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,49 @@
 import "./App.css";
 import logo from "./logo.svg";
+import Lobby from "./components/Lobby";
+import Layout from "./components/Layout";
+import ErrorPage from "./components/ErrorPage";
 import Match from "./components/Match";
+import GameForm from "./components/GameForm";
+import RockPaperSccissors from "./components/RockPaperSccissors";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Match />
-      </header>
-    </div>
-  );
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Lobby />,
+      },
+      {
+        path: "match/:matchId/player1/:player1/rounds/:rounds",
+        element: <Lobby />,
+      },
+      {
+        path: "matching/player1/:player1/player2/:player2/secondPlayerJoined/:secondPlayerJoined",
+        element: <Match />,
+      },
+      {
+        path: "playing/player1/:player1/player2/:player2/rounds/:rounds/match/:matchId/isFromPlayer1/:isFromPlayer1",
+        element: <RockPaperSccissors />
+      },
+      {
+        path: "playing/player1/:player1/player2/:player2/rounds/:rounds/match/:matchId/",
+        element: <RockPaperSccissors />
+      }
+    ]
+  },
+  {
+    path: "match/:matchId/player1/:player1/rounds/:rounds",
+    element: <Lobby />,
+  },
+]);
+
+const App = () => <RouterProvider router={router} />
 
 export default App;
