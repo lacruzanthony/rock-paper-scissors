c66568309bb7d282609943180dcbfa6e2b0e27cb
Anthony Lacruz <lacruzanthony20@gmail.com>
2022-12-25 22:55:52 -0500

    client: create error page

---
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <Link to={`/`}>Go to lobby</Link>
+    </div>
+  );
+}
+
+export default ErrorPage;
